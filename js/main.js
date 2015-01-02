4c8ec20445f7e9b82356da625775ce3677998a25
Brendan Martin <brendan.martin@jacquette.com>
2015-01-02 15:46:55 -0500

    reengineering for mobile first

--- js/main.js
+++ js/main.js
@@ -2,22 +2,6 @@ $(document).ready(function(){
 	var viewport = 	$(window), contact = $('.contact_form');
 
 
-
-	// NAVIGATION
-	// 
-	// 
-	// added .off("click")
-	// stops redundant clicks
-	// $('.mobile_nav_hamburger').off("click").on('click', function(){
-	// 	$('.mobile_nav').toggle();
-	// });
-
-	// $('.body, .mobile_nav_links').off("click").on('click', function(){
-	// 	$('.mobile_nav').hide();
-	// });
-	
-	
-
 	viewport.on({
 		scroll:function() {
 			console.log(viewport.scrollTop());
